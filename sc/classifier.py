8156b17db849e28e173ddea0ede4d1a991b1ef55
changkong <changkong@mogujie.com>
2020-03-26 15:12:24 +0800

    add skew features

--- sc/classifier.py
+++ sc/classifier.py
@@ -179,6 +179,7 @@ class Classifier(object):
         feature_dict['up_edges'] = self.getUpEdges_(signals, params)
         feature_dict['down_edges'] = self.getDownEdges_(signals, params)
 
+
         # combined features
         if not feature_masks or 'peaks_num' in feature_masks:
             feature_dict['peaks_num'] = len(feature_dict['peaks'])
@@ -203,6 +204,7 @@ class Classifier(object):
             feature_dict['down_edge_height'] = self.getEdgeHeight_(signals, feature_dict['down_edges'])
         if not feature_masks or ['up_edges', 'down_edges'] <= feature_masks:
             feature_dict['paired_edges'] = self.getPairedEdges_(params, feature_dict['up_edges'], feature_dict['down_edges'])
+
         # 上下沿对比数据
         feature_dict['paired_edge_height'] = self.getPairedEdgeHeight_(signals, feature_dict['paired_edges'])
         feature_dict['paired_edge_height_diff'] = sorted(self.getPairedEdgeDifference_(feature_dict['paired_edge_height']), reverse=True)
@@ -231,6 +233,13 @@ class Classifier(object):
         else:
             feature_dict['skewness_mean'] = 0
             feature_dict['skewness_delta'] = 0
+
+        # 获取波形上沿落差统计分布
+        feature_dict['skew_level_dist'] = self.getSkewLevelFeatures(raw_signals, feature_dict['peaks'])
+        feature_dict['skew_level_1'] = feature_dict['skew_level_dist'][0]
+        feature_dict['skew_level_2'] = feature_dict['skew_level_dist'][1]
+        feature_dict['skew_level_3'] = feature_dict['skew_level_dist'][2]
+        feature_dict['skew_level_4'] = feature_dict['skew_level_dist'][3]
 
         # 获取上下沿边的长度diff分位数据
         if len(feature_dict['paired_edge_height_diff']) != 0:
@@ -267,6 +276,7 @@ class Classifier(object):
             feature_dict['width_diff_20'] = 100
             feature_dict['width_diff_30'] = 100
             feature_dict['width_diff_50'] = 100
+
 
         return feature_dict
 
@@ -669,16 +679,37 @@ class Classifier(object):
             max_cyclic_pairs = max(max_cyclic_pairs, cyclic_pair)
         return max_cyclic_pairs
 
-   # def cyclicIntense(self, seqs, interval):
-   #     """
-   #     using auto self-correlation to calculate cyclic informations
-   #     """
-   #     periodic_pairs = 0.0
-   #     for i in range(0, len(seqs) - interval):
-   #         if seqs[i] == 1 and seqs[i + interval] == 1:
-   #             periodic_pairs += 1
-   #     #s = pd.Series(seqs)
-   #     #ret = s.autocorr(lag = interval)
-   #     #if pd.isna(ret):
-   #     #    ret = 0
-   #     return periodic_pairs
+
+    def plateau_point(self, raw_signals, peak_point, direction=-1):
+        def end_conditions(x, direction, signals):
+            return x >= 0 if direction < 0 else x < len(signals)
+        last_height = raw_signals[peak_point]
+        last_i = peak_point
+        next_i = peak_point + direction
+        while end_conditions(next_i, direction, raw_signals):
+            height_diff = abs(raw_signals[next_i] - last_height)
+            if height_diff < 0.04:
+                return next_i
+            last_height = raw_signals[next_i]
+            last_i = next_i
+            next_i = next_i + direction
+        return last_i
+
+    def getSkewLevelFeatures(self, raw_signal, peaks):
+        skew_level_1 = 0
+        skew_level_2 = 0
+        skew_level_3 = 0
+        skew_level_4 = 0
+        for i in peaks:
+            left_height = raw_signal[self.plateau_point(raw_signal, i - 1, -1)]
+            right_height = raw_signal[self.plateau_point(raw_signal, i - 1, 1)]
+            height_diff = abs(left_height - right_height)
+            if height_diff <= 0.02:
+                skew_level_1 += 1
+            elif height_diff <= 0.05:
+                skew_level_2 += 1
+            elif height_diff <= 0.1:
+                skew_level_3 += 1
+            else:
+                skew_level_4 += 1
+        return [skew_level_1, skew_level_2, skew_level_3, skew_level_4]
--- sc/model.py
+++ sc/model.py
@@ -7,5 +7,6 @@ import logging
 logger = logging.getLogger('server')
 
 ModelVersionFeatureConfig = {
-        "gbdt_smooth_signal": {"features": ['peaks_num', 'down_peaks_num', 'up_edges_num', 'down_edges_num', 'peak_edge_ratio', 'down_peak_edge_ratio','edge_diff_10', 'edge_diff_20', 'width_diff_10', 'negative_peak_num', 'max_down_peak_point', 'inter_diff_mean', 'inter_diff_delta','skewness_mean', 'skewness_delta', 'cyclic_intense_nopeak', 'cyclic_intense_downpeak'], "path": "gbdt_model"}
+        "gbdt_smooth_signal": {"features": ['peaks_num', 'down_peaks_num', 'up_edges_num', 'down_edges_num', 'peak_edge_ratio', 'down_peak_edge_ratio','edge_diff_10', 'edge_diff_20', 'width_diff_10', 'negative_peak_num', 'max_down_peak_point', 'inter_diff_mean', 'inter_diff_delta','skewness_mean', 'skewness_delta', 'cyclic_intense_nopeak', 'cyclic_intense_downpeak'], "path": "gbdt_model"},
+        "skew_enhance": {"features": ['peaks_num', 'down_peaks_num', 'up_edges_num', 'down_edges_num', 'peak_edge_ratio', 'down_peak_edge_ratio','edge_diff_10', 'edge_diff_20', 'width_diff_10', 'negative_peak_num', 'max_down_peak_point', 'inter_diff_mean', 'inter_diff_delta','skewness_mean', 'skewness_delta', 'cyclic_intense_nopeak', 'cyclic_intense_downpeak', 'skew_level_1', 'skew_level_2', 'skew_level_3', 'skew_level_4'], "path": "gbdt_model"}
 }
